9d0a8ba1e482f7b636593b1a2a67df0140c0dc4c
knuppe22 <knuppe22@gmail.com>
2018-03-31 22:57:40 +0900

    [row, column] to [column, row]

--- Dohmoku/Game.cs
+++ Dohmoku/Game.cs
@@ -11,7 +11,7 @@ namespace Dohmoku
 	public class Game
 	{
 		public Team player;					// Current player
-		int[,] board = new int[19, 19];		// Gomoku board, 0: empty, 1: black, 2: white
+		int[,] board = new int[19, 19];		// Gomoku board. Location is [column#, row#]. 0: empty, 1: black, 2: white
 		Team current = Team.Black;			// Current playing player
 		Team? winner = null;				// Winner of the game. Initialized by null.
 
@@ -23,17 +23,17 @@ namespace Dohmoku
 			while (winner == null)
 			{
 				DrawBoard();
-				Console.WriteLine(current.ToString("g") + " team's turn. Enter coordinate to place stone with the format of \"i j\"."); // TODO: Change to j 10, make exception handling
+				Console.WriteLine(current.ToString("g") + " team's turn. Enter coordinate to place stone with the format of \"x y\"."); // TODO: Change to j 10, make exception handling
 				string str = Console.ReadLine();
 				string[] val = str.Split(' ');
-				int x = int.Parse(val[0]);
-				int y = int.Parse(val[1]);
+				int x = int.Parse(val[0]) - 1;
+				int y = int.Parse(val[1]) - 1;
 				if (x < 0 || x > 18 || y < 0 || y > 18)
 				{
 					Console.WriteLine("Out of index! Enter again.");
 					continue;
 				}
-				if (!IsPlacable(x, y))
+				else if (!IsPlacable(x, y))
 				{
 					Console.WriteLine("You cannot place here! Enter again.");
 					continue;
@@ -41,30 +41,34 @@ namespace Dohmoku
 				Place(current, x, y);
 				current = (Team)(((int)current + 1) % 2);
 			}
+
+			Console.Clear();
+			DrawBoard();
+			Console.WriteLine(winner.Value.ToString("g") + " wins!");
 		}
 		
-		void DrawBoard()
+		void DrawBoard()		// Method drawing gomoku board
 		{
-			Console.Write("ij");
+			Console.Write("yx");
 			for (int i = 0; i < 19; i++)
 			{
-				Console.Write(i.ToString("D2"));
+				Console.Write(" " + (char)('A' + i));
 			}
 			Console.WriteLine();
 
 			for (int i = 0; i < 19; i++)
 			{
-				Console.Write(i.ToString("D2"));
+				Console.Write((i + 1).ToString("D2"));
 				for (int j = 0; j < 19; j++)
 				{
-					Console.Write(Int2Char(board[i, j]));
+					Console.Write(Int2Char(board[j, i]));
 				}
 
 				Console.WriteLine();
 			}
 		}
 
-		string Int2Char(int n)
+		string Int2Char(int n)	// For DrawBoard method
 		{
 			switch(n)
 			{
@@ -79,11 +83,11 @@ namespace Dohmoku
 			}
 		}
 
-		bool IsPlacable(int x, int y)
+		bool IsPlacable(int x, int y)	// Is board[x, y] is zero?
 		{
 			return board[x, y] == 0;
 		}
-		void Place(Team team, int x, int y)
+		void Place(Team team, int x, int y)	// Place team's stone in board[x, y]
 		{
 			if (IsPlacable(x, y))
 			{
